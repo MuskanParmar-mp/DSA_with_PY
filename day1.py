c9a6b7e07352e99977b00fbd49e5725c4e2f4970
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-07-29 15:08:28 +0530

    pyDSA

--- day1.py
+++ day1.py
@@ -1,4 +1,6 @@
 name = input("Enter your name : ")
 num = int(input("Enter your Age : "))
+per = float(input("enter your percentage"))
+#print(f"Your name is {name} and your age is {num} and  your percentage is {per:.2f}")
 
-print(f"Your name is {name} and your age is {num}")
+print(f"My name is {name}. I am  {num} years old and I scored {per} percentage in 12th")
