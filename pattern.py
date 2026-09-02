f52eba362cf5fd69114412dc21468384900a4bc6
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-09-02 15:18:01 +0530

    dsapy

--- pattern.py
+++ pattern.py
@@ -3,4 +3,10 @@ for i in range(1,n+1):                    #1,2,3,4,5
     for x in range(range(i)):             # i=1 - 0 i=2-0, 1
         print("*", end="")
 
-    print()    
+    print()   
+
+
+for i in range(1, n+1):
+    for x in range(1,i+1):
+        print(x,end=" ")
+    print()     
